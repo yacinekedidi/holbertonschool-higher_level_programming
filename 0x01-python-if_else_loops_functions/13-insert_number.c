9b6d288a80f8ba6a9591e48fe4611124d764202b
yacinekedidi <kedidiyacine@gmail.com>
2020-04-28 21:08:08 +0100

    C

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_node - check the code for Holberton School students.
+ * @head: linked list
+ * @number: nbr
+ * Return: Always 0.
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+listint_t *loop = *head, *new = malloc(sizeof(listint_t));
+
+if (!new)
+return (NULL);
+
+new->n = number;
+new->next = NULL;
+
+if (!*head || loop->n > new->n)
+{
+*head = new;
+return (*head);
+}
+
+
+while (loop)
+{
+if (!loop->next || new->n < loop->next->n)
+{
+new->next = loop->next;
+loop->next = new;
+return (loop);
+}
+
+loop = loop->next;
+}
+
+return (NULL);
+}
