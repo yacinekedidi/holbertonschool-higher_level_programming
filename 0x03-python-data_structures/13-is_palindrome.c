4ea105de0e08363820c8c96b06c51b30e0e659f4
yacinekedidi <kedidiyacine@gmail.com>
2020-05-05 14:26:21 +0100

    C

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,4 +1,27 @@
 #include "lists.h"
+
+/**
+ * test - check the code for Holberton School students.
+ * @head: linked list
+ * @tab: table
+ * Return: Always .
+ */
+int test(listint_t **head, int tab[])
+{
+int i = 0;
+while (*head)
+{
+if (tab[i] == (*head)->n)
+{
+i++;
+*head = (*head)->next;
+}
+else
+return (0);
+}
+return (1);
+}
+
 /**
  * reverse - check the code for Holberton School students.
  * @head: linked list
@@ -53,18 +76,8 @@ i++;
 
 *head = reverse(*head);
 
-i = 0;
-while (*head)
-{
-if (tab[i] == (*head)->n)
-{
-i++, *head = (*head)->next;
-}
-else
-{
+i = test(head, tab);
+
 free(tab);
-return (0);
-}
-} free(tab);
-return (1);
+return (i);
 }
