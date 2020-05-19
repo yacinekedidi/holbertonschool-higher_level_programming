a7617c5d854762c6f9970a630e0c20e25ba4d0c1
yacinekedidi <kedidiyacine@gmail.com>
2020-05-19 18:14:03 +0100

    Py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -6,9 +6,11 @@ class Square:
     """defines a square."""
     def __init__(self, size=0):
         self.size = size
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -16,8 +18,10 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
         return self.__size ** 2
+
     def my_print(self):
         if not self.__size:
             print("")
