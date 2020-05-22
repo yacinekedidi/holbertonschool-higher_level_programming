9ad4f8f26b369b70a11de14f4feea8926c7b685d
yacinekedidi <kedidiyacine@gmail.com>
2020-05-22 21:19:48 +0100

    Py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,21 +1,35 @@
 #!/usr/bin/python3
-"""module for tests."""
+"""Unittest for max_integer([..])
 
 
+
+"""
 import unittest
 
 max_integer = __import__('6-max_integer').max_integer
 
-class TestMax_Integer(unittest.TestCase):
+
+class TestMaxInteger(unittest.TestCase):
+
     def test_result(self):
-        """test_result function."""
+        """
+        test_result function.
+        """
         self.assertEqual(max_integer([10, 20, 5]), 20)
         self.assertEqual(max_integer([20, 10, 5]), 20)
         self.assertEqual(max_integer([10, 10, 20]), 20)
         self.assertEqual(max_integer([20, 20, 20]), 20)
         self.assertEqual(max_integer([-10, -10, -10]), -10)
         self.assertEqual(max_integer([0.4, 5.5, 10.5]), 10.5)
+        self.assertEqual(max_integer([]), None)
 
     def test_errors(self):
-        """test_errors functions."""
-        self.assertRaises(TypeError, max_integer([100, "a", 5]))
+        with self.assertRaises(TypeError):
+            max_integer([10, "a", 5])
+
+        with self.assertRaises(TypeError):
+            max_integer(3)
+        self.assertRaises(TypeError, max_integer, [0.5, "help"])
+
+if __name__ == '__main__':
+    unittest.main("tests/6-max_integer_test.py")
