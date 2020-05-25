510fadd3b8fba793056e45c3967563a995cc5a18
yacinekedidi <kedidiyacine@gmail.com>
2020-05-25 18:27:06 +0100

    fix

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -11,8 +11,8 @@ class Rectangle:
     Defines a rectangle
     """
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     def __str__(self):
         s = ""
