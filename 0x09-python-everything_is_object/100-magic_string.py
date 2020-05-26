807d61bef459935e076030ab1f92eb00a0593b81
yacinekedidi <kedidiyacine@gmail.com>
2020-05-26 21:10:08 +0100

    Py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 def magic_string():
     setattr(magic_string, "i", getattr(magic_string, "i", -1) + 1)
-    return "Holberton, " * i + "Holberton"
+    return "Holberton, " * magic_string.i + "Holberton"
