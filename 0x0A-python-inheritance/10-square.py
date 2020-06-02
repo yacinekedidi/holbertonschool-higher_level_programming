7c47812ede5480fc60a6232a9d4801d5f6051812
yacinekedidi <kedidiyacine@gmail.com>
2020-06-02 18:18:04 +0100

    f

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -12,5 +12,6 @@ class Square(Rectangle):
     subclass
     """
     def __init__(self, size):
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
