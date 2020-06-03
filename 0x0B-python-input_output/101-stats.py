ceeb8fee7d0a873b95ec11305b7a9cc0986e24c3
yacinekedidi <kedidiyacine@gmail.com>
2020-06-04 00:54:57 +0100

    fix

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -22,7 +22,10 @@ if __name__ == "__main__":
             else:
                 nbl += 1
             line = line.split()
-            s += int(line[-1])
+            try:
+                s += int(line[-1])
+            except ValueError:
+                pass
             for k, v in d.items():
                 if k == line[-2]:
                     d[k] += 1
