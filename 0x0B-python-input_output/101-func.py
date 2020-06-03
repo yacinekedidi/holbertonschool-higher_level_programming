1f23b526a088ece87f7d989fd18c37e2e4f9d64e
yacinekedidi <kedidiyacine@gmail.com>
2020-06-03 22:10:04 +0100

    Py

--- 0x0B-python-input_output/101-func.py
+++ 0x0B-python-input_output/101-func.py
@@ -9,9 +9,7 @@ def print_stat(s, d):
     """
     print the stats and size.
     """
-    try:
-        print("File size: {}".format(s))
-        for k, v in d.items():
+    print("File size: {}".format(s))
+    for k, v in d.items():
+        if v != 0:
             print("{}: {}".format(k, v))
-    except Exception:
-        pass
