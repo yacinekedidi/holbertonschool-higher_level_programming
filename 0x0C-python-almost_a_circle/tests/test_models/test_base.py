543c14899155af652c70df6f62e8f27169e717f2
yacinekedidi <kedidiyacine@gmail.com>
2020-06-09 12:49:39 +0100

    test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -14,6 +14,7 @@ class TestBase(unittest.TestCase):
                 """test without args"""
                 b1 = Base()
                 b2 = Base()
+                self.assertEqual(b1.id, 1)
                 self.assertEqual(b1.id, b2.id - 1)
 
 if __name__ == '__main__':
