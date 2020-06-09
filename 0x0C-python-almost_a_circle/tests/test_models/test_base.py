5b37cd283ef92bcee3f5eb4f2655adb739b67110
yacinekedidi <kedidiyacine@gmail.com>
2020-06-09 12:55:35 +0100

    test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -9,13 +9,20 @@ from models.square import Square
 
 class TestBase(unittest.TestCase):
         """class"""
+        def setUp(self):
+                """setup"""
+                Base._base__nb_objects = 0
+                pass
+
+        def tearDown(self):
+                """tears down"""
+                pass
 
         def test_no_args(self):
                 """test without args"""
                 b1 = Base()
                 b2 = Base()
-                self.assertEqual(b1.id, 1)
                 self.assertEqual(b1.id, b2.id - 1)
-
+                self.assertEqual(Base._Base__nb_objects, b2.id)
 if __name__ == '__main__':
     unittest.main()
