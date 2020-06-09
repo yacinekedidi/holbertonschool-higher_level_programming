aff3d4ee1369b2c40c468a0240e9516abace9b11
yacinekedidi <kedidiyacine@gmail.com>
2020-06-09 14:02:55 +0100

    Py

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -7,14 +7,52 @@ from models.rectangle import Rectangle
 from models.square import Square
 
 
-class TestBase(unittest.TestCase):
+class TestSquare_instantiation(unittest.TestCase):
         """class"""
-
         def test_no_args(self):
                 """test without args"""
-                b1 = Square(5)
-                b2 = Square(5)
-                self.assertEqual(b1.id, b2.id - 1)
+                with self.assertRaises(TypeError):
+                        Square()
+
+        def test_one_arg(self):
+                """one arg"""
+                r1 = Square(10)
+                r2 = Square(12)
+                self.assertEqual(r1.id, r2.id - 1)
+                self.assertEqual(r1.size, 10)
+
+        def test_two_args(self):
+                """two args"""
+                r1 = Square(10, 12)
+                r2 = Square(10, 2)
+                self.assertEqual(r1.id, r2.id - 1)
+                self.assertEqual(r1.size, 10)
+                self.assertEqual(r1.x, 12)
+
+        def test_three_args(self):
+                """two args"""
+                r1 = Square(10, 2, 12)
+                r2 = Square(10, 2, 3)
+                self.assertEqual(r1.id, r2.id - 1)
+                self.assertEqual(r1.size, 10)
+                self.assertEqual(r1.x, 2)
+                self.assertEqual(r1.y, 12)
+
+        def test_four_args(self):
+                """two args"""
+                r1 = Square(10, 2, 12, 6)
+                r2 = Square(10, 2, 0, 7)
+                self.assertEqual(r1.size, 10)
+                self.assertEqual(r1.x, 2)
+                self.assertEqual(r1.y, 12)
+                self.assertEqual(r1.id, 6)
+                self.assertEqual(r2.id, 7)
+
+        def test_more(self):
+                """two args"""
+                with self.assertRaises(TypeError):
+                        r1 = Square(10, 2, 12, 10, 5)
+
 
 if __name__ == '__main__':
     unittest.main()
