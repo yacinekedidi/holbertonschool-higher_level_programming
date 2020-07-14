b92a69c030411c7bfac8c008e5f0eac0337da276
yacinekedidi <kedidiyacine@gmail.com>
2020-07-14 14:17:53 +0100

    sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,7 +2,6 @@
 -- (in the database hbtn_0d_usa) on your MySQL server.
 -- QUERY
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
--- QUERY
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (id INT NOT NULL UNIQUE AUTO_INCREMENT,
 state_id INT NOT NULL,
 name VARCHAR(256) NOT NULL,
