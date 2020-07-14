c1acf1a7bc797a919b6513b055b2351bd893943d
yacinekedidi <kedidiyacine@gmail.com>
2020-07-14 14:15:26 +0100

    sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 -- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server
 -- QUERY
 SHOW GRANTS FOR user_0d_1@localhost;
--- Query
 SHOW GRANTS FOR user_0d_2@localhost;
