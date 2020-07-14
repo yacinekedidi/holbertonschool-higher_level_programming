16819b4c35bb736ecafc5207a0f59fa9dba1a4cd
yacinekedidi <kedidiyacine@gmail.com>
2020-07-14 17:08:31 +0100

    sql

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,9 @@
+-- script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+-- QUERY
+SELECT ts.title, SUM(tsr.rate) as ratings
+FROM tv_shows AS ts
+LEFT JOIN
+tv_show_ratings AS tsr
+ON ts.id = tsr.show_id
+GROUP BY ts.title
+ORDER BY ratings DESC;
