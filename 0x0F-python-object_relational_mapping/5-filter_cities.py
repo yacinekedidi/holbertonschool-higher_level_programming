80cd2ae886d6b0afec7d7e6da9ae29ae1357bc3c
yacinekedidi <kedidiyacine@gmail.com>
2020-08-08 12:44:03 +0100

    py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,14 +9,15 @@ if __name__ == '__main__':
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
     cur.execute("""SELECT DISTINCT (c.name) FROM cities AS c
-                   LEFT JOIN states AS s
+                   INNER JOIN states AS s
                    ON c.state_id = s.id
                    WHERE s.name = %s
                    ORDER BY c.id ASC
                 """, (argv[4],))
     query_rows = cur.fetchall()
-    for i in range(len(query_rows)):
-        print(list(query_rows[i])[0], end=", "
-              if i < len(query_rows) - 1 else "\n")
+    l = []
+    for i in query_rows:
+        l.append(i[0])
+    print(*l, sep=", ")
     cur.close()
     conn.close()
