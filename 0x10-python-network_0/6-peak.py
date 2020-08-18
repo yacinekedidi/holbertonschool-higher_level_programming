d8e3d75f5885aeaf33b3acc697475c335a94ae9d
yacinekedidi <kedidiyacine@gmail.com>
2020-08-18 19:27:16 +0100

    new

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,6 +11,10 @@ def find_peak(l):
     if len(l) == 2:
         return max(l)
 
+    if len(l) > 2:
+        if l[0] >= l[1] and l[0] == max(l):
+            return l[0]
+
     m = len(l) // 2
     if l[m] >= l[m - 1] and l[m] >= l[m + 1]:
         return l[m]
