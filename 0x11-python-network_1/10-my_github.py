e969155036ed1dc66f047298e3d85c73ac90a1cb
yacinekedidi <kedidiyacine@gmail.com>
2020-08-24 20:13:27 +0100

    new

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,5 +6,5 @@ argv = sys.argv
 
 
 if __name__ == "__main__":
-     r = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
-     print(r.json().get("id"))
+    r = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
+    print(r.json().get("id"))
