fe05d45dab45793697c3edca1a2560a86519768d
yacinekedidi <kedidiyacine@gmail.com>
2020-08-24 20:37:43 +0100

    new

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module"""
+import requests
+import sys
+argv = sys.argv
+
+
+if __name__ == "__main__":
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1]))
+    c = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(c[i].get("sha"), c[i].get("commit").get("author").get("name")))
+    except:
+        pass
