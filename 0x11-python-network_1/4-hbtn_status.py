6262eaa3b168dd842ce933695109d254e76e9843
yacinekedidi <kedidiyacine@gmail.com>
2020-08-24 19:28:59 +0100

    new

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ import requests
 
 if __name__ == "__main__":
     r = requests.get("https://intranet.hbtn.io/status")
-    
+
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
