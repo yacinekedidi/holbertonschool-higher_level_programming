417b2d49d5f9df99444198a5dd52032cbbe1c885
yacinekedidi <kedidiyacine@gmail.com>
2020-08-24 20:01:15 +0100

    new

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,12 +7,15 @@ argv = sys.argv
 
 if __name__ == "__main__":
    try:
-        x = argv[1]
+        x = argv[1][0]
     except:
         x = ""
     value = {"q": x}
     r = requests.post("http://0.0.0.0:5000/search_user", data=value)
-    if r.json():
-        print("[{}] {}".format(r.json().get("id"), r.json().get("name")))
-    else:
-        print("No result")
+    try:
+        if r.json():
+            print("[{}] {}".format(r.json().get("id"), r.json().get("name")))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
