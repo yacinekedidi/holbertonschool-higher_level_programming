ea337a62b4095c36ff30d5f464bd3bda875d7f7a
yacinekedidi <kedidiyacine@gmail.com>
2020-08-24 19:15:32 +0100

    new

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,9 +6,9 @@ argv = sys.argv
 
 
 if __name__ == "__main__":
-	req = request.Request(argv[1])
-	try:
-    	with request.urlopen(req) as response:
-        	print(response.read().decode("utf-8"))
+    req = request.Request(argv[1])
+    try:
+        with request.urlopen(req) as response:
+            print(response.read().decode("utf-8"))
     except error.HTTPError as e:
-    	print("Error code: {}".format(e.code))
+        print("Error code: {}".format(e.code))
