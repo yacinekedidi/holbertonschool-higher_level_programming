3e84b59ffa2c6a97b5e55bec3bc09d7a103ac3b3
yacinekedidi <kedidiyacine@gmail.com>
2020-08-24 20:39:42 +0100

    new

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,10 +6,12 @@ argv = sys.argv
 
 
 if __name__ == "__main__":
-    r = requests.get('https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1]))
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(argv[2], argv[1]))
     c = r.json()
     try:
         for i in range(10):
-            print("{}: {}".format(c[i].get("sha"), c[i].get("commit").get("author").get("name")))
+            print("{}: {}".format(c[i].get("sha"), 
+                                  c[i].get("commit").get("author").get("name")))
     except:
         pass
