f274b9e716f73dd86b9229e81d4650785603427d
yacinekedidi <kedidiyacine@gmail.com>
2020-09-15 17:22:12 +0100

    js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,11 @@ const myArgs = process.argv.slice(2);
 if (myArgs.length <= 1) {
   console.log(0);
 } else {
-  myArgs.sort().reverse();
-  console.log(myArgs[1]);
+  for (let i = 0; i < myArgs.length; i++) {
+    if (Number(myArgs[i]) === Math.max(...myArgs)) {
+      myArgs.splice(i, 1);
+      break;
+    }
+  }
+  console.log(Math.max(...myArgs));
 }
