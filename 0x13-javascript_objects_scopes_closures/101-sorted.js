41aa805c6b9310c2d42f730eb395fe91eefb8987
yacinekedidi <kedidiyacine@gmail.com>
2020-09-21 13:44:51 +0100

    js

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const d = require('./101-data').dict;
+const l = Object.values(d);
+var ll = l.filter(function (item, pos) {
+  return l.indexOf(item) === pos;
+});
+
+const dd = {};
+const keys = Object.keys(d);
+
+for (const i in ll) {
+  const x = [];
+  for (const j in keys) {
+    if (d[keys[j]] === ll[i]) {
+      x.push(keys[j]);
+      dd[ll[i]] = x;
+    }
+  }
+}
+console.log(dd);
