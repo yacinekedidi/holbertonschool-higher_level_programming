16136fa62e106a811cd85285b2f1fc1ef613b138
yacinekedidi <kedidiyacine@gmail.com>
2020-09-22 19:30:18 +0100

    js

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const request = require('request');
+const myArgs = process.argv.slice(2);
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + myArgs[0];
+
+request(url, function (err, resp, body) {
+  err && console.log(err);
+  const info = JSON.parse(body);
+  for (let i = 0; i < info.characters.length; i++) {
+    request(info.characters[i], function (err, resp, body) {
+      err && console.log(err);
+      const character = JSON.parse(body);
+      console.log(character.name);
+    });
+  }
+});
